69f2b8125a05a70296ca14e40254cc634d6cd71d
Anton Morokin <anton.morokin@gmai.com>
2022-03-04 21:54:24 +0300

    Added data repository.

---
+++ WebApi/Data/ICarRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using WebApi.Model;
+
+namespace WebApi.Data
+{
+    public interface ICarRepository
+    {
+        Task<Car> GetCarByNumberAsync(string carNumber);
+
+        Task<Car> GetCarByIdAsync(int carId);
+    }
+}
